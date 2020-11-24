c80bf9b28f1e3ddaab838d1574317cd34ae44eaa
Gianni LaMartina <68284481+giannilamartina@users.noreply.github.com>
2020-11-24 14:45:09 -0600

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Pet {
+   private String dogBreed;
+
+   public void setBreed(String userBreed) {
+      dogBreed = userBreed;
+   }
+
+   public String getBreed() {
+      return dogBreed;
+   }
+}
